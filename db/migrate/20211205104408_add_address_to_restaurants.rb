2781e752adbc3e1f457ad6051dec32be2427bb83
sean meager <seanmeager10@gmail.com>
2021-12-05 10:47:13 +0000

    add methods to change

--- db/migrate/20211205104408_add_address_to_restaurants.rb
+++ db/migrate/20211205104408_add_address_to_restaurants.rb
@@ -1,5 +1,9 @@
 class AddAddressToRestaurants < ActiveRecord::Migration[6.1]
   def change
     add_column :restaurants, :address, :string
+    change_column :restaurants, :address, :string
+    rename_column :restaurants, :address, :string
+    remove_column :restaurants, :address, :string
+    add_reference :restaurants, :address, :string
   end
 end
